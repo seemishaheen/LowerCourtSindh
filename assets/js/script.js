affb35d9001bfa9d6a04d11197c5e61a0f98d7b6
seemishaheen <seemis343@gmail.com>
2024-05-21 10:07:11 +0500

    html template

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$('.slick').slick({
+
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  vertical: true,
+});
+$('.slick-2').slick({
+
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  // vertical:true,
+});
